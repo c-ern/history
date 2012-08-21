b6614fa7b505bc9a4e65ad51b071118750133b4e
Steve Lair <steve.lair@gmx.de>
2012-08-21 18:01:17 +0200

    mit seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Chancellor.create(:first_name => 'Konrad', :last_name => 'Adenauer', :birthday => '05.01.1876', :day_of_death => '19.04.1967', :inauguration => '15.09.1949')
+Chancellor.create(:first_name => 'Ludwig', :last_name => 'Erhard', :birthday => '04.02.1897', :day_of_death => '05.05.1977', :inauguration => '16.10.1963')
+Chancellor.create(:first_name => 'Kurt Georg', :last_name => 'Kiesinger', :birthday => '06.04.1904', :day_of_death => '09.03.1988', :inauguration => '01.12.1966')
+Chancellor.create(:first_name => 'Willy', :last_name => 'Brandt', :birthday => '18.12.1913', :day_of_death => '08.10.1992', :inauguration => '21.10.1969')
+Chancellor.create(:first_name => 'Walter', :last_name => 'Scheel', :birthday => '08.09.1919', :inauguration => '07.05.1974')
+Chancellor.create(:first_name => 'Helmut', :last_name => 'Schmidt', :birthday => '23.12.1918', :inauguration => '16.05.1974')
